9f651d04b1a0de0198726461a6038f0b0585431c
Rudy <drawncloser@gmail.com>
2018-12-09 18:07:54 -0600

    simplify server script

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/rudy-test'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/rudy-test/index.html'));
+app.get('/*', function(req,res) { 
+  res.sendFile(path.join(__dirname+'/dist/rudy-test/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
